c2e59944182f319e44422a32472e7679595c4c1c
NaumZakletskyi <naum.zakletskyi@sysgears.com>
2024-02-27 10:54:58 +0200

    Added baseUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseApiUrl = process.env.API_URL
+export const baseApiUrl = 'http://localhost:3000'
 export const ownApi = axios.create({
   baseURL: baseApiUrl,
 });
